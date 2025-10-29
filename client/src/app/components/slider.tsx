03c014b27859f7afa67db565258b04ef774802be
Angel Yang <angelyang@Angels-MacBook-Air.local>
2025-10-29 13:55:40 -0700

    add slider component

---
+++ client/src/app/components/slider.tsx
@@ -0,0 +1,47 @@
+"use client"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { slide } from "./testimonials";
+import { Pagination, Autoplay } from "swiper/modules";
+import Image from "next/image";
+import { urlFor } from "../util";
+
+interface SwiperType {
+    slides: slide[]
+}
+export default function Slider({ slides }: SwiperType) {
+
+    const slidesContent = slides.map(({image, paragraph, label}, index) => (
+        <SwiperSlide key={index}>
+            <div className="bg-white p-10 h-[410px]">
+                <div className="flex justify-center pb-5">
+                    <Image src={urlFor(image).url()} alt="" width={25} height={25}/>
+                </div>
+                <p className="font-montserrat text-[16px] h-[250px]">{paragraph}</p>
+                <div className="font-montserrat text-[22px] pt-5 border-t-2 border-[#e3dfd6]">{label}</div>
+            </div>
+        </SwiperSlide>
+    ))
+
+    return (
+        <Swiper
+            className="h-[465px] lg:h-[480px]"
+            modules={[Pagination, Autoplay]}
+            pagination={{ clickable: true }}
+            autoplay={{
+                delay: 10000, // 3 seconds
+                disableOnInteraction: false, // continue autoplay after user interacts
+                reverseDirection: true,
+            }}
+            spaceBetween={50}
+            slidesPerView={1}
+            onSlideChange={() => console.log("slide change")}
+            // Add breakpoints for responsiveness if needed
+            breakpoints={{
+                640: { slidesPerView: 2, spaceBetween: 20 },
+                1024: { slidesPerView: 3, spaceBetween: 40 },
+                }}
+            >
+            <>{slidesContent}</>
+        </Swiper>
+    )
+}
