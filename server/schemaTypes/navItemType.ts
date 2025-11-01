ff4a324dce9e679d937de68e5736c50d76b40b3d
Angel Yang <angelyang@Angels-MacBook-Air.local>
2025-11-01 13:44:51 -0700

    add post to nav reference

--- server/schemaTypes/navItemType.ts
+++ server/schemaTypes/navItemType.ts
@@ -14,11 +14,11 @@ export const navItemType = defineType({
         name: 'reference',
         type: 'reference',
         title: 'Reference',
-        to: [{ type: 'page' }, { type: 'slider' }], // Example: Link to 'page' or 'post' documents
+        to: [{ type: 'page' }, { type: 'slider' }, { type: 'post' }], // Example: Link to 'page' or 'post' documents
         description: 'Alternatively, link to a Sanity document.',
       }),
       defineField({
-        name: 'children',
+        name: 'subNavItems',
         type: 'array',
         title: 'Sub-navigation Items',
         of: [{ type: 'navigationItem' }], // Allow nested navigation
