b55b84871593de69e32ea0fffef8661720309776
yamogocha <yamogocha@yahoo.com>
2025-11-03 17:48:09 -0800

    Revert "Sanity"

--- server/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import {postType} from './postType'
-import {navType} from './navType'
-import {navItemType} from './navItemType'
-import {pageType} from './pageType'
-import { sliderType } from './sliderType'
-import { slideType } from './slideType'
-
-export const schemaTypes = [postType, navType, navItemType, pageType, slideType, sliderType]
