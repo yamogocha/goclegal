07f80c0dc4ebf53d6728f982f7660da2242940df
Angel Yang <angelyang@Angels-MacBook-Air.local>
2025-11-07 23:43:47 -0800

    fix overlay not extended to bottom in about page

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -4,16 +4,16 @@ import AboutComponent, { AboutPage } from "../components/about";
 import Navigation from "../navigation/page";
 import AwardsAndHonors from "../rewards-and-honors/page";
 
-const ABOUT_PAGE_QUERY = groq`*[_type == "page" && slug.current == "about"][0]{title, description, "image": image.asset->url, "photo": photo.asset->url, body}`
+const ABOUT_PAGE_QUERY = groq`*[_type == "page" && slug.current == "about"][0]{headline, subHeadline, "image": image.asset->url, "photo": photo.asset->url, body}`
 
 export default async function About() {
     const aboutPage = await client.fetch<AboutPage>(ABOUT_PAGE_QUERY)
 
     return (
-        <>
+        <div className="relative min-h-screen">
             <Navigation />
             <AboutComponent {...aboutPage} />
             <AwardsAndHonors />
-        </>
+        </div>
     )
 }
