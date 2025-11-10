fa368d159583d4ae63629861ed735eabc75b677a
Angel Yang <angelyang@Angels-MacBook-Air.local>
2025-11-09 23:47:43 -0800

    add footer type

--- server/schemaTypes/index.ts
+++ server/schemaTypes/index.ts
@@ -1,9 +1,9 @@
 import {postType} from './postType'
-import {navType} from './navType'
-import {navItemType} from './navItemType'
+import {navType, navItem} from './navType'
 import {pageType} from './pageType'
 import { sliderType } from './sliderType'
 import { slideType } from './slideType'
 import { contactType } from './contactType'
+import { footerType, information } from './footerType'
 
-export const schemaTypes = [postType, navType, navItemType, pageType, slideType, sliderType, contactType]
+export const schemaTypes = [postType, navType, navItem, pageType, slideType, sliderType, contactType, footerType, information]
--- server/schemaTypes/navType.ts
+++ server/schemaTypes/navType.ts
@@ -2,6 +2,31 @@
 // import { GrNavigate } from "react-icons/gr";
 import { defineField, defineType } from 'sanity';
 
+export const navItem = defineType({
+  name: 'navigationItem',
+  title: 'Navigation Item',
+  type: 'object',
+  fields: [
+    defineField({
+      name: 'label',
+      type: 'string',
+      title: 'Label',
+    }),
+    defineField({
+      name: 'reference',
+      type: 'reference',
+      title: 'Reference',
+      to: [{ type: 'page' }, { type: 'slider' }, { type: 'post' }, { type: 'contact' }], // Example: Link to 'page' or 'post' documents
+      description: 'Alternatively, link to a Sanity document.',
+    }),
+    defineField({
+      name: 'subNavItems',
+      type: 'array',
+      title: 'Sub-navigation Items',
+      of: [{ type: 'navigationItem' }], // Allow nested navigation
+    }),
+  ],
+});
 
 export const navType = defineType({
   name: 'navigation',
