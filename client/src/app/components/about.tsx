ce1f0a3c8b95501fc04ec72b075bc56870431309
Angel Yang <angelyang@Angels-MacBook-Air.local>
2025-11-09 22:23:15 -0800

    add scroll auto when see less is clicked

--- client/src/app/components/about.tsx
+++ client/src/app/components/about.tsx
@@ -38,11 +38,17 @@ export default function AboutComponent(aboutPage: AboutPage) {
             />
         )
     }
-    const toggleReadMore = () => setReadMore(!readMore)
+    const toggleReadMore = () => {
+        setReadMore(!readMore)
+        if (readMore) {
+            const element = document.getElementById("greg-photo")
+            element?.scrollIntoView({ behavior: "auto" })
+        }
+    }
 
     return (
         <div className="bg-[#00305bcf] w-full h-full">
-            <Image src={image} alt="About page background image" fill className="object-cover -z-5" />
+            <Image id="greg-photo" src={image} alt="About page background image" fill className="object-cover -z-5" />
             <MotionWrapper className="w-full lg:w-2/3 m-auto h-[400px] px-5 flex flex-col justify-center item-center text-center text-white">
                 <h1 className='text-[36px] lg:text-[48px] leading-tight lg:leading-normal font-bold pb-6'>{headline}</h1>
                 <p className="font-montserrat font-medium text-[18px] lg:text-[22px]">{subHeadline}</p>
