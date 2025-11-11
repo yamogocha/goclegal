77d1e070f23eb40a9a6ff900b2c1e82b929c3169
Angel Yang <angelyang@Angels-MacBook-Air.local>
2025-11-11 10:56:16 -0800

    add svgs socail icons

--- client/src/app/assets/scalesOfJustice.tsx
+++ client/src/app/assets/scalesOfJustice.tsx
@@ -1,6 +1,5 @@
-export type SVGType = {
-    className: string
-}
+import { SVGType } from "./trophy";
+
 export default function ScalesOfJustice({ className }: SVGType) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
