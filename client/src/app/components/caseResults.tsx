6e7409113d92d9f0174fb75d1335c7710bfbca39
Angel Yang <angelyang@Angels-MacBook-Air.local>
2025-11-13 17:52:48 -0800

    add margin for mobile case results swiper

--- client/src/app/components/caseResults.tsx
+++ client/src/app/components/caseResults.tsx
@@ -36,7 +36,7 @@ export default function CaseResultComponent({ caseResultsPage, caseResultsSlider
     const bottomContent = body.slice(2) 
     
     const cards = slides.map(({label, paragraph}, index) => (
-        <SwiperSlide key={index} className="mt-6 mb-14">
+        <SwiperSlide key={index} className="mt-6 mb-20 lg:mb-14">
             <div className="bg-white border border-[#00305b] rounded lg:w-[85%] m-auto h-[450px] lg:h-[300px] p-10 flex flex-col items-center justify-center">
                 <div className="text-[#00305b] font-montserrat font-medium leading-tight lg:leading-normal text-center text-[22px] pb-6">{label}</div>
                 <p className="text-[#00305b] font-montserrat text-[16px] lg:text-[18px] text-center">{paragraph}</p>
