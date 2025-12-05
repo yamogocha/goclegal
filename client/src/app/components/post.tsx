59a08aaa5d535d1dfc4fb496bc1a4627adb74f7b
Angel Yang <angelyang@Angels-MacBook-Air.local>
2025-12-04 21:52:36 -0800

    add image updatedAt to clear cache

--- client/src/app/components/post.tsx
+++ client/src/app/components/post.tsx
@@ -10,18 +10,19 @@ export type Post = {
     headline: string
     subHeadline: string
     image: string
+    imageUpdatedAt: string
     columnLeft: PortableTextBlock[]
     columnRight: PortableTextBlock[]
     buttonText: string
     phoneNumber: string
 }
 export default function PostComponent(postQuery: Post) {
-    const { headline, subHeadline, image, columnLeft, columnRight, buttonText, phoneNumber } = postQuery
+    const { headline, subHeadline, image, imageUpdatedAt, columnLeft, columnRight, buttonText, phoneNumber } = postQuery
 
     return(
         <div className="bg-[#00305bcf] w-full h-full">
             <div className="relative w-full h-[300px] lg:h-[400px]">
-                <Image src={image} alt="Auto Accidents background image" fill className='object-cover -z-5'/>
+                <Image src={`${image}?t=${imageUpdatedAt}`} alt="Auto Accidents background image" fill className='object-cover -z-5'/>
                 <MotionWrapper className="w-full lg:w-2/3 m-auto h-[300px] lg:h-[400px] px-5 flex flex-col justify-center item-center text-center text-white">
                     <h1 className='text-[36px] lg:text-[48px] leading-tight lg:leading-normal font-bold pb-6'>{headline}</h1>
                     <p className="font-montserrat font-medium text-[18px] lg:text-[22px]">{subHeadline}</p>
