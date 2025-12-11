9e76adfa534c059fd361e129fc1ab8783eb6833c
Angel Yang <angelyang@Angels-MacBook-Air.local>
2025-12-10 19:30:29 -0800

    update practice area pages to populate dynamic schema and metadata

--- client/src/app/[slug]/page.tsx
+++ client/src/app/[slug]/page.tsx
@@ -21,20 +21,23 @@ export async function generateStaticParams() {
 
 export async function generateMetadata({ params }: Params) {
     const { slug } = await params
-    return buildPageMetadata(slug)
+    const postParams = await client.fetch(`*[_type == "post" && slug.current == $slug][0]{title, description, "image": image.asset->url, "slug": $slug}`,{ slug })
+    return buildPageMetadata(postParams)
 }
 
 export default async function PracticeAreas({ params }: Params) {
     const { slug } = await params
     const AUTO_ACCIDENTS_QUERY = groq`*[_type == "post" && slug.current == $slug][0]
-{title, headline, subHeadline, date, "image": image.asset->url, "imageId": image.asset->_id, columnLeft, columnRight, buttonText, phoneNumber}`
+{title, description, headline, subHeadline, date, "image": image.asset->url, "imageId": image.asset->_id, columnLeft, columnRight, buttonText, phoneNumber}`
+    
 
     const postQuery = await client.fetch<Post>(AUTO_ACCIDENTS_QUERY,{ slug })
+    const postParams = await client.fetch(`*[_type == "post" && slug.current == $slug][0]{title, description, "image": image.asset->url, "slug": $slug}`,{ slug })
 
     return (
         <div className="relative min-h-screen">
             <Script id={`${slug}-schema`} type="application/ld+json">
-                {JSON.stringify(buildPracticeAreaSchema(slug))}
+                {JSON.stringify(buildPracticeAreaSchema(postParams))}
             </Script>
             <Navigation />
             <PostComponent {...postQuery} />
