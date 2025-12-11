0c408a48a5fd96ded42e9823c13b7565fbdcf983
Angel Yang <angelyang@Angels-MacBook-Air.local>
2025-12-10 19:32:11 -0800

    update blogs to populate dynamic schema and metadata

--- client/src/app/blog/[slug]/page.tsx
+++ client/src/app/blog/[slug]/page.tsx
@@ -1,8 +1,9 @@
+import Script from "next/script"
 import PostComponent, { Post } from "../../components/post"
 import Contact from "../../contact/page"
 import Footer from "../../footer/page"
 import Navigation from "../../navigation/page"
-// import { buildPageMetadata } from "../../util/schema"
+import { buildBlogSchema, buildPageMetadata } from "../../util/schema"
 import { client } from "@/sanity/client"
 import { groq } from "next-sanity"
 
@@ -16,22 +17,24 @@ export async function generateStaticParams() {
     return slugs.map((slug: string) => ({ slug }))
 }
 
-// export async function generateMetadata({ params }: Params) {
-//     const { slug } = await params
-//     return buildPageMetadata(slug)
-// }
+export async function generateMetadata({ params }: Params) {
+    const { slug } = await params
+    const postParams = await client.fetch(`*[_type == "post" && slug.current == $slug][0]{title, description, "image": image.asset->url, "slug": $slug}`,{ slug })
+    return buildPageMetadata(postParams)
+}
 
 export default async function Blog({ params }: Params) {
     const { slug } = await params
     const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]
     {title, headline, subHeadline, date, "image": image.asset->url, "imageId": image.asset->_id, columnLeft, columnRight, buttonText, phoneNumber}`
     const postQuery = await client.fetch<Post>(POST_QUERY, { slug })
+    const postParams = await client.fetch(`*[_type == "post" && slug.current == $slug][0]{title, description, "image": image.asset->url, "slug": $slug, date}`,{ slug })
 
     return (
         <div className="relative min-h-screen">
-            {/* <Script id={`${slug}-schema`} type="application/ld+json">
-                {JSON.stringify(buildPracticeAreaSchema(slug))}
-            </Script> */}
+            <Script id={`${slug}-schema`} type="application/ld+json">
+                {JSON.stringify(buildBlogSchema(postParams))}
+            </Script>
             <Navigation />
             <PostComponent {...postQuery} />
             <Contact />
